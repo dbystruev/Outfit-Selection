98af6946148fe075096d54c154926dbda616470c
Denis Bystruev <github@learnSwift.ru>
2019-06-19 11:16:00 +0300

    Initial Commit

---
+++ Outfit Selection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Outfit Selection
+//
+//  Created by Denis Bystruev on 19/06/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
