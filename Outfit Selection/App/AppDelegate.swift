44e7f229efda79ff7e0f8bb4078cfe63850ae2b7
Denis Bystruev <dbystruev@me.com>
2020-12-08 16:29:10 +0300

    Override dark mode with light

--- Outfit Selection/App/AppDelegate.swift
+++ Outfit Selection/App/AppDelegate.swift
@@ -25,6 +25,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
+        // Ignore dark mode
+        if #available(iOS 13.0, *) {
+            window?.overrideUserInterfaceStyle = .light
+        }
+        
         // For tests
         
         return true
