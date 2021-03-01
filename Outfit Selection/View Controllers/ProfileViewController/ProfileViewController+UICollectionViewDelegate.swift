a1481a0766bf2edd005423d608461e5a03ddd6cc
Denis Bystruev <dbystruev@me.com>
2021-03-01 13:53:27 +0300

    Profile: gender change warning

--- Outfit Selection/View Controllers/ProfileViewController/ProfileViewController+UICollectionViewDelegate.swift
+++ Outfit Selection/View Controllers/ProfileViewController/ProfileViewController+UICollectionViewDelegate.swift
@@ -21,12 +21,28 @@ extension ProfileViewController: UICollectionViewDelegate {
             let newGender = Gender.allCases[indexPath.row]
             guard let currentGender = Gender.current, newGender != shownGender else { return }
             if currentGender != newGender {
-                debug("Change of gender from \(currentGender.rawValue) to \(newGender.rawValue) will clear wishlists")
+                let message = "Gender change will clear wishlists"
+                let title = "Change to \(newGender.rawValue)"
+                let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                let cancel = UIAlertAction(title: "Don't", style: .cancel) { _ in
+                    // Keep gender to current
+                    self.shownGender = Gender.current
+                    collectionView.reloadSections([0])
+                }
+                let ok = UIAlertAction(title: "Clear", style: .destructive) { _ in
+                    // Reload gender section with new gender
+                    self.shownGender = newGender
+                    collectionView.reloadSections([0])
+                }
+                alert.addAction(ok)
+                alert.addAction(cancel)
+                present(alert, animated: true)
+            } else {
+                if newGender != shownGender {
+                    shownGender = newGender
+                    collectionView.reloadSections([0])
+                }
             }
-            
-            // Reload gender section with new gender
-            shownGender = newGender
-            collectionView.reloadSections([0])
         case 1:
             // Section 1 is brands — reuse brands view controller to action
             brandsViewController?.collectionView(collectionView, didSelectItemAt: indexPath)
