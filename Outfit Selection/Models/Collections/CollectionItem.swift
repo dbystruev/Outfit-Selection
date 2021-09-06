f0eaac92396b71dfafa8dea9f7930aa1e67fedb5
Denis Bystruev <dbystruev@me.com>
2021-09-06 11:44:45 +0300

    Implemented collection item model

--- Outfit Selection/Models/Collections/CollectionItem.swift
+++ Outfit Selection/Models/Collections/CollectionItem.swift
@@ -8,7 +8,8 @@
 
 struct CollectionItem {
     
-    /// Kind (type) of the collection item
+    // MARK: - Types
+    /// There are 2 types of collections: with items or with looks of items
     enum Kind: CustomStringConvertible {
         case item
         case look
@@ -24,5 +25,23 @@ struct CollectionItem {
                 return ".look"
             }
         }
+    }
+    
+    // MARK: - Stored Properties
+    /// Kind (type) of the collection item
+    let kind: Kind
+    
+    /// Single item or a few items depending on collection item type
+    let items: [Item]
+    
+    // MARK: - Init
+    init(item: Item) {
+        kind = .item
+        items = [item]
+    }
+    
+    init(look: [Item]) {
+        kind = .look
+        items = look
     }
 }
