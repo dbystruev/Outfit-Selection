8b272734b3eba09b681d31b97fd3e2c41517a35c
Denis Bystruev <dbystruev@me.com>
2021-09-20 21:38:41 +0300

    Alert: refactored configured to include multi action titles, styles, and handlers

--- Outfit Selection/Managers/Alert.swift
+++ Outfit Selection/Managers/Alert.swift
@@ -13,23 +13,25 @@ enum Alert {
     /// Configure and return UI alert controller
     /// - Parameters:
     ///   - title: alert title
-    ///   - message: alert message
-    ///   - handler: action to call when this action is selected
+    ///   - message: alert messages
+    ///   - actionTitles: action titles
+    ///   - styles: alert action styles
+    ///   - handlers: actions to call when correspoinding actions are selected
     /// - Returns: configured  UI alert controller
-    static func configure(_ title: String?, message: String? = nil, handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertController {
+    static func configured(
+        _ title: String?,
+        message: String? = nil,
+        actionTitles: [String] = ["OK"],
+        styles: [UIAlertAction.Style] = [.default],
+        handlers: [(UIAlertAction) -> Void] = []
+    ) -> UIAlertController {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
-        // If handler does not exist add ok
-        guard let handler = handler else {
-            let ok = UIAlertAction(title: "OK", style: .default)
-            alert.addAction(ok)
-            return alert
-        }
-        
-        // If it does, add cancel and yes buttons
-        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
-        let yes = UIAlertAction(title: "Yes", style: .default, handler: handler)
-        for action in [cancel, yes] {
+        for index in 0 ..< actionTitles.count {
+            // Create and add the action
+            let action = UIAlertAction(
+                title: actionTitles[index], style: styles[safe: index] ?? .default, handler: handlers[safe: index]
+            )
             alert.addAction(action)
         }
         
@@ -42,40 +44,48 @@ enum Alert {
     ///   - handler: the handler to be called after user confirms the action and the item has been removed
     /// - Returns: the configured UI alert controller
     static func dislike(_ item: Item?, handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertController {
-        configure("Dislike", message: "Do you really want to dislike the item?") { action in
-            Item.dislike(item)
-            handler?(action)
-        }
+        configured(
+            "Dislike",
+            message: "Do you really want to dislike the item?",
+            actionTitles: ["Yes", "Cancel"],
+            styles: [.default, .cancel],
+            handlers: [{ action in
+                Item.dislike(item)
+                handler?(action)
+            }]
+        )
     }
     
     static func genderChange(
         to newGender: Gender,
         sender: ProfileViewController
     ) -> UIAlertController {
-        let message = "Gender change will reload items"
-        let title = "Change to \(newGender.rawValue)"
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let cancel = UIAlertAction(title: "Don't", style: .cancel) { _ in
-            // Keep gender to current
-            sender.shownGender = Gender.current
-            sender.profileCollectionView.reloadSections([0])
-        }
-        let ok = UIAlertAction(title: "Change", style: .destructive) { _ in
-            // Reload gender section with new gender
-            Gender.current = newGender
-            guard let tabBarController = sender.tabBarController as? TabBarController else {
-                debug("Error: can't cast", sender.tabBarController, "to TabBarConroller")
-                return
-            }
-            tabBarController.popToProgress()
-        }
-        alert.addAction(ok)
-        alert.addAction(cancel)
-        return alert
+        configured(
+            "Change to \(newGender.rawValue)",
+            message: "Gender change will reload items",
+            actionTitles: ["Don't", "Change"],
+            styles: [.cancel, .destructive],
+            handlers: [{ _ in
+                // Keep gender to current
+                sender.shownGender = Gender.current
+                sender.profileCollectionView.reloadSections([0])
+            }, { _ in
+                // Reload gender section with new gender
+                Gender.current = newGender
+                guard let tabBarController = sender.tabBarController as? TabBarController else {
+                    debug("Error: can't cast", sender.tabBarController, "to TabBarConroller")
+                    return
+                }
+                tabBarController.popToProgress()
+            }]
+        )
     }
     
     // MARK: - Static Computed Properties
     static var noItems: UIAlertController {
-        configure("Can't create collection", message: "Please add some items or outfits to the wishlists by liking them before creating a collection")
+        configured(
+            "Can't create collection",
+            message: "Please add some items or outfits to the wishlists by liking them before creating a collection"
+        )
     }
 }
