fb171616486816d12b72b23f1e27a1ddea2140ce
Denis Bystruev <dbystruev@me.com>
2021-09-21 23:24:24 +0300

    Occasions: `height for row at` and `did select row at` delegates for table view

---
+++ Outfit Selection/View Controllers/Occasions/OccasionsViewController/OccasionsViewController+UITableViewDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  OccasionsViewController+UITableViewDelegate.swift
+//  Outfit Selection
+//
+//  Created by Denis Bystruev on 09.09.2021.
+//  Copyright © 2021 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+extension OccasionsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat { OccasionCell.heigth }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        Occasion.all[indexPath.row].isSelected.toggle()
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+}
