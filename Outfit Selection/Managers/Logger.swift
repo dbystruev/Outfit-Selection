1ab6bd1d9d89c7560d680d98eadf32b09923f2fd
Denis Bystruev <dbystruev@me.com>
2022-03-07 13:12:01 +0300

    v0.7.8 build 2022.03.07

--- Outfit Selection/Managers/Logger.swift
+++ Outfit Selection/Managers/Logger.swift
@@ -31,7 +31,7 @@ class Logger {
     }()
     
     /// Duration in seconds when cache should be used
-    static let cacheDuration: TimeInterval = 3600 // 0.1
+    static let cacheDuration: TimeInterval = 360 // 0.1
     
     /// Load logs from filesystem files
     private static let filesystemLogs: [String: String] = {
